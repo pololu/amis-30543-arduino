d06a97a3b9d6bba0d342a69ca11c9788b1a06e48
David (Pololu) <dev-david@pololu.com>
2015-05-18 20:54:31 -0700

    Simplified the example a lot and finally got rid of the A-Star stuff.

--- examples/StepTest/StepTest.ino
+++ examples/StepTest/StepTest.ino
@@ -1,37 +1,18 @@
 #include <SPI.h>
 #include <AMIS30543.h>
-#include <AStar32U4Prime.h>  // TODO: remove
 
 const uint8_t amisSlaveSelect = 10;
 const uint8_t amisStepPin = 9;
 
 AMIS30543 stepper;
 
-AStar32U4PrimeButtonA buttonA;
-AStar32U4PrimeButtonB buttonB;
-AStar32U4PrimeButtonC buttonC;
-
 void setup()
 {
-  delay(1000);
-  Serial.begin(9600);
-  Serial.println("start");
-
-  SPI.begin();  //initialize SPI
-  stepper.init(amisSlaveSelect);
-
+  SPI.begin();
   pinMode(amisStepPin, OUTPUT);
+  delay(1);
 
-  Serial.println("AMIS SPI test");
-  while (1)
-  {
-    if (buttonB.getSingleDebouncedRelease())
-    {
-      Serial.println("button pushed");
-      break;
-    }
-  }
-
+  stepper.init(amisSlaveSelect);
   stepper.setCurrentMilliamps(500);
   stepper.enableDriver();
 }
