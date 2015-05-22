8590b4b6bffab18e863971d3b5e79279669f34b0
David (Pololu) <dev-david@pololu.com>
2015-05-21 20:26:01 -0700

    BasicStepping: Made it use the DIR pin.

--- examples/BasicStepping/BasicStepping.ino
+++ examples/BasicStepping/BasicStepping.ino
@@ -1,11 +1,11 @@
 /* This example shows basic use of the AMIS-30543 stepper motor
 driver.
 
-It shows how to initialize the dirver, set the current limit, set
+It shows how to initialize the driver, set the current limit, set
 the micro-stepping mode, and enable the driver.  It shows how to
-send pulses to the step pin to get the driver to take steps and
-how to switch directions over SPI.  The DO and DIR pins are not
-used and do not need to be connected.
+send pulses to the NXT/STEP pin to get the driver to take steps
+and how to switch directions using the DIR pin.  The DO pin is
+not used and does not need to be connected.
 
 Before using this example, be sure to change the
 setCurrentMilliamps line to have an appropriate current limit for
@@ -20,26 +20,42 @@ information about how to connect the driver:
 
 const uint8_t amisSlaveSelect = 4;
 const uint8_t amisStepPin = 5;
+const uint8_t amisDirPin = 6;
 
 AMIS30543 stepper;
 
 void setup()
 {
   SPI.begin();
+  stepper.init(amisSlaveSelect);
+
+  // Drive the NXT/STEP and DIR pins low initially.
+  digitalWrite(amisStepPin, LOW);
   pinMode(amisStepPin, OUTPUT);
+  digitalWrite(amisDirPin, LOW);
+  pinMode(amisDirPin, OUTPUT);
+
+  // Give the driver some time to power up.
   delay(1);
 
-  stepper.init(amisSlaveSelect);
+  // Reset the driver to its default settings.
   stepper.resetSettings();
-  stepper.setCurrentMilliamps(245);
+
+  // Set the current limit.  You should change the number here to
+  // an appropriate value for your particular system.
+  stepper.setCurrentMilliamps(135);
+
+  // Set the number of microsteps that correspond to one full step.
   stepper.setStepMode(32);
+
+  // Enable the motor outputs.
   stepper.enableDriver();
 }
 
 void loop()
 {
   // Step in the default direction 1000 times.
-  stepper.setDirection(0);
+  setDirection(0);
   for (unsigned int x = 0; x < 1000; x++)
   {
     step();
@@ -49,7 +65,7 @@ void loop()
   delay(300);
 
   // Step in the other direction 1000 times.
-  stepper.setDirection(1);
+  setDirection(1);
   for (unsigned int x = 0; x < 1000; x++)
   {
     step();
@@ -59,15 +75,31 @@ void loop()
   delay(300);
 }
 
+// Sends a pulse on the NXT/STEP pin to tell the driver to take
+// one step, and also delays to control the speed of the motor.
 void step()
 {
-  // Send a pulse on the NXT/STEP pin to tell the driver to take one step.
+  // The NXT/STEP minimum high pulse width is 2 microseconds.
   digitalWrite(amisStepPin, HIGH);
   delayMicroseconds(3);
   digitalWrite(amisStepPin, LOW);
   delayMicroseconds(3);
 
   // The delay here controls the stepper motor's speed.  You can
-  // increase the delay to make the stepper motor go slower.
+  // increase the delay to make the stepper motor go slower.  If
+  // you decrease the delay, the stepper motor will go fast, but
+  // there is a limit to how fast it can go before it starts
+  // missing steps.
   delayMicroseconds(2000);
+}
+
+// Writes a high or low value to the direction pin to specify
+// what direction to turn the motor.
+void setDirection(bool dir)
+{
+  // The NXT/STEP pin must not change for at least 0.5
+  // microseconds before and after changing the DIR pin.
+  delayMicroseconds(1);
+  digitalWrite(amisDirPin, dir);
+  delayMicroseconds(1);
 }
